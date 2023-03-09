4ac37300e6a936b384ab0daeaaa13210051e59f9
unknown <raouf-bar@live.fr>
2023-03-10 00:46:36 +0100

    all

---
+++ app.js
@@ -0,0 +1,199 @@
+let galoory = [
+    {
+        tite: "High - Back Bench",
+        prand: "Ikea",
+        img: "./images/f-1.jpg",
+        prix: "19$",
+        color: ["#f15025", "#222"],
+        text: "Cloud bread VHS hell of banjo bicycle rights jianbing umami mumblecore etsy 8-bit pok pok +1 wolf. Vexillologist yr dreamcatcher waistcoat, authentic chillwave trust fund. Viral typewriter fingerstache pinterest pork belly narwhal. Schlitz venmo everyday carry kitsch pitchfork chillwave iPhone taiyaki trust fund hashtag kinfolk microdosing gochujang live-edge",
+        id: 1
+    },
+    {
+        tite: "Albany Table",
+        prand: "Marcos",
+        img: "./images/f-2.jpg",
+        prix: "79.99$",
+        color: ["#57bc67", "#eee"],
+        text: "Cloud bread VHS hell of banjo bicycle rights jianbing umami mumblecore etsy 8-bit pok pok +1 wolf. Vexillologist yr dreamcatcher waistcoat, authentic chillwave trust fund. Viral typewriter fingerstache pinterest pork belly narwhal. Schlitz venmo everyday carry kitsch pitchfork chillwave iPhone taiyaki trust fund hashtag kinfolk microdosing gochujang live-edge",
+        id: 2
+
+    },
+    {
+        tite: "Accent Chair",
+        prand: "Caressa",
+        img: "./images/f-3.jpg",
+        prix: "25.99$",
+        color: ["blue", "green", "yellow"],
+        text: "Cloud bread VHS hell of banjo bicycle rights jianbing umami mumblecore etsy 8-bit pok pok +1 wolf. Vexillologist yr dreamcatcher waistcoat, authentic chillwave trust fund. Viral typewriter fingerstache pinterest pork belly narwhal. Schlitz venmo everyday carry kitsch pitchfork chillwave iPhone taiyaki trust fund hashtag kinfolk microdosing gochujang live-edge",
+        id: 3
+    },
+    {
+        tite: "Wooden Table",
+        prand: "Caressa",
+        img: "./images/f-4.jpg",
+        prix: "45.99$",
+        color: ["blue", "red"],
+        text: "Cloud bread VHS hell of banjo bicycle rights jianbing umami mumblecore etsy 8-bit pok pok +1 wolf. Vexillologist yr dreamcatcher waistcoat, authentic chillwave trust fund. Viral typewriter fingerstache pinterest pork belly narwhal. Schlitz venmo everyday carry kitsch pitchfork chillwave iPhone taiyaki trust fund hashtag kinfolk microdosing gochujang live-edge",
+        id: 4
+    },
+    {
+        tite: "Dining Table",
+        prand: "Caressa",
+        img: "./images/f-5.jpg",
+        prix: "6.99$",
+        color: ["blue", "orange"],
+        text: "Cloud bread VHS hell of banjo bicycle rights jianbing umami mumblecore etsy 8-bit pok pok +1 wolf. Vexillologist yr dreamcatcher waistcoat, authentic chillwave trust fund. Viral typewriter fingerstache pinterest pork belly narwhal. Schlitz venmo everyday carry kitsch pitchfork chillwave iPhone taiyaki trust fund hashtag kinfolk microdosing gochujang live-edge",
+        id: 5
+    },
+    {
+        tite: "Sofa Set",
+        prand: "Liddy",
+        img: "./images/f-6.jpg",
+        prix: "69.99$",
+        color: ["blue"],
+        text: "Cloud bread VHS hell of banjo bicycle rights jianbing umami mumblecore etsy 8-bit pok pok +1 wolf. Vexillologist yr dreamcatcher waistcoat, authentic chillwave trust fund. Viral typewriter fingerstache pinterest pork belly narwhal. Schlitz venmo everyday carry kitsch pitchfork chillwave iPhone taiyaki trust fund hashtag kinfolk microdosing gochujang live-edge",
+        id: 6
+    },
+    {
+        tite: "Modern Bookshelf",
+        prand: "Marcos",
+        img: "./images/f-7.jpg",
+        prix: "8.99$",
+        color: ["green", "yellow", "red"],
+        text: "Cloud bread VHS hell of banjo bicycle rights jianbing umami mumblecore etsy 8-bit pok pok +1 wolf. Vexillologist yr dreamcatcher waistcoat, authentic chillwave trust fund. Viral typewriter fingerstache pinterest pork belly narwhal. Schlitz venmo everyday carry kitsch pitchfork chillwave iPhone taiyaki trust fund hashtag kinfolk microdosing gochujang live-edge",
+        id: 7
+    },
+    {
+        tite: "Emperor Bed",
+        prand: "Liddy",
+        img: "./images/f-8.jpg",
+        prix: "21.99$",
+        color: ["orange"],
+        text: "Cloud bread VHS hell of banjo bicycle rights jianbing umami mumblecore etsy 8-bit pok pok +1 wolf. Vexillologist yr dreamcatcher waistcoat, authentic chillwave trust fund. Viral typewriter fingerstache pinterest pork belly narwhal. Schlitz venmo everyday carry kitsch pitchfork chillwave iPhone taiyaki trust fund hashtag kinfolk microdosing gochujang live-edge",
+        id: 8
+    },
+    {
+        tite: "Utopia Sofa",
+        prand: "Marcos",
+        img: "./images/f-9.jpg",
+        prix: "39.99$",
+        color: ["green", "blue"],
+        text: "Cloud bread VHS hell of banjo bicycle rights jianbing umami mumblecore etsy 8-bit pok pok +1 wolf. Vexillologist yr dreamcatcher waistcoat, authentic chillwave trust fund. Viral typewriter fingerstache pinterest pork belly narwhal. Schlitz venmo everyday carry kitsch pitchfork chillwave iPhone taiyaki trust fund hashtag kinfolk microdosing gochujang live-edge",
+        id: 9
+    },
+    {
+        tite: "Entertainment Center",
+        prand: "Liddy",
+        img: "./images/f-10.jpg",
+        prix: "29.99$",
+        color: ["green", "red"],
+        text: "Cloud bread VHS hell of banjo bicycle rights jianbing umami mumblecore etsy 8-bit pok pok +1 wolf. Vexillologist yr dreamcatcher waistcoat, authentic chillwave trust fund. Viral typewriter fingerstache pinterest pork belly narwhal. Schlitz venmo everyday carry kitsch pitchfork chillwave iPhone taiyaki trust fund hashtag kinfolk microdosing gochujang live-edge",
+        id: 10
+    },
+    {
+        tite: "Utopia Sofa",
+        prand: "Marcos",
+        img: "./images/f-11.jpg",
+        prix: "39.99$",
+        color: ["green", "blue"],
+        text: "Cloud bread VHS hell of banjo bicycle rights jianbing umami mumblecore etsy 8-bit pok pok +1 wolf. Vexillologist yr dreamcatcher waistcoat, authentic chillwave trust fund. Viral typewriter fingerstache pinterest pork belly narwhal. Schlitz venmo everyday carry kitsch pitchfork chillwave iPhone taiyaki trust fund hashtag kinfolk microdosing gochujang live-edge",
+        id: 11
+    },
+    {
+        tite: "Leather Sofa",
+        prand: "Liddy",
+        img: "./images/f-12.jpg",
+        prix: "9.99$",
+        color: ["black"],
+        text: "Cloud bread VHS hell of banjo bicycle rights jianbing umami mumblecore etsy 8-bit pok pok +1 wolf. Vexillologist yr dreamcatcher waistcoat, authentic chillwave trust fund. Viral typewriter fingerstache pinterest pork belly narwhal. Schlitz venmo everyday carry kitsch pitchfork chillwave iPhone taiyaki trust fund hashtag kinfolk microdosing gochujang live-edge",
+        id: 512
+    }
+];
+
+let row = document.querySelector(".galory")
+let oneP = document.querySelector(".one-p")
+
+
+
+window.addEventListener("DOMContentLoaded", () => {
+    let content = galoory.map((e) => {
+        return `<div class="ra col-lg-4 col-sm-6 col-md-6 mt-3 text-center mb-3" data-id="${e.id}">
+                <div class="img" style="height: 250px; overflow: hidden; width: 100%;">
+                    <img style="height: 100%;width: 100%;" class="img-fluid mb-2 rounded-2" src="${e.img}" alt="">
+                </div>
+
+                <h6 class="text-black-50 mt-3">${e.tite}</h6>
+                <div class="prix fw-bold">${e.prix}</div>
+            </div>`
+    }).join("");
+    row.innerHTML = content;
+
+    let ra = document.querySelectorAll(".ra");
+
+    kach(ra);
+})
+
+
+console.log(galoory);
+
+let all = document.querySelector(".all")
+let alls = document.querySelector(".sall")
+let btn = document.querySelector(".main-btn")
+function kach(se) {
+    se.forEach((e) => {
+        e.addEventListener("click", () => {
+            let id = e.dataset.id
+            let idp = galoory.filter((e) => {
+                return e.id == id ? e : "";
+            });
+            idp = idp[0];
+            desplayer()
+            let Scontent = `<div class="col-lg-6 mb-4 d-flex justify-content-center">
+                <img class="img" src="${idp.img}" alt="">
+            </div>
+            <div class="col-lg-6 mb-4">
+                <h2 class="fw-normal">${idp.tite}</h2>
+                <h3 class="text-black-50 fs-4">${idp.prand}</h3>
+                <span class="fw-bold fs-5 mb-2 d-block">${idp.prix}</span>
+                <div class = "color "></div>
+                <p>${idp.text}</p>
+                <button class="main-btn rounded-3 pb-1 pt-1 pe-3 ps-3  text-light fs-5">add to cart</button>
+            </div>`
+            oneP.innerHTML = Scontent;
+            let color = document.querySelector(".color");
+            for (let i = 0; i < idp.color.length; i++) {
+                let span = document.createElement("span");
+                span.className = "colors";
+                span.style.backgroundColor = idp.color[i];
+                color.appendChild(span);
+            }
+        })
+    })
+}
+
+btn.addEventListener("click", () => {
+    desplayer()
+})
+
+
+function desplayer() {
+    all.classList.toggle("non")
+    alls.classList.toggle("non")
+}
+
+{/* <div class="col-lg-6 mb-4 d-flex justify-content-center">
+                <img class="img" src="./images/f-11.jpg" alt="">
+            </div>
+            <div class="col-lg-6 mb-4">
+                <h2 class="fw-normal">Albany Table</h2>
+                <h3 class="text-black-50 fs-4">Marcos</h3>
+                <span class="fw-bold fs-5 mb-2 d-block">79.99</span>
+                <p>Cloud bread VHS hell of banjo bicycle rights jianbing umami mumblecore etsy 8-bit pok pok +1 wolf.
+                    Vexillologist yr
+                    dreamcatcher waistcoat, authentic chillwave trust fund. Viral typewriter fingerstache pinterest pork
+                    belly narwhal.
+                    Schlitz venmo everyday carry kitsch pitchfork chillwave iPhone taiyaki trust fund hashtag kinfolk
+                    microdosing gochujang
+                    live-edge</p>
+                <button class="main-btn rounded-3 pb-1 pt-1 pe-3 ps-3  text-light fs-5">add to cart</button>
+            </div> */}
